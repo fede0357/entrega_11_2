57893b4968c78273a9763739f85500d21f7469d0
Fede-357 <fedelarra.07@gmail.com>
2023-10-01 15:42:20 +0200

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,19 +1,32 @@
-const URL = `//jsonplaceholder.typicode.com/users`;
+const URL = `https://jsonplaceholder.typicode.com/users`;
 const form = document.getElementById('formulario');
-const nombre = document.getElementById('name').value;
-const apellido = document.getElementById('apellido').value;
-const nacimiento = document.getElementById('fechaNacimiento').value;
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const nombre = document.getElementById('name').value;
+    const apellido = document.getElementById('apellido').value;
+    const nacimiento = document.getElementById('fechaNacimiento').value;
 
-form.addEventListener("submit", () => {
+    
     const data = {
         nombre,
         apellido,
         nacimiento
-    }
-});
+    };
 
-fetch(URL, {
-    method: "post",
-    
-})
+    fetch(URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(resultado => {
+        console.log(resultado); 
+    })
+    .catch(error => {
+        alert('Error');
+    });
+});
